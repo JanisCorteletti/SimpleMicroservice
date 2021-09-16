764b9293610199460f8c2a532cd1104a94d6a865
DESKTOP-HIQOAFN\janis <janis.corteletti@cytosmart.com>
2021-09-16 15:04:45 +0200

    16092021

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Database.Entities
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source = DESKTOP-HIQOAFN\SQLEXPRESS; initial catalog=UserMicroservice; persist security info=True; User ID = adm; Password = adm");
+        }
+    }
+}
